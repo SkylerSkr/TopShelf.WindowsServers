19359e5e22ce431455da6ea7fc6ce1b9f88a3287
TangoShi <tangoshi@humanbacker.com>
2019-04-25 11:28:23 +0800

    feature/fisrtcommit
    第一次提交 使用topshelf搭建windows服务

---
+++ TopShelf.WindowsServers/TopShelf.Job/SendMailJob.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Service.Mail;
+using Service.MQ;
+
+namespace TopShelf.Job
+{
+    public class SendMailJob
+    {
+        public void Process()
+        {
+            //拉取fanout_queue_Mail队列的消息，并绑定到fanout_Mail交换机，routingkey为mail
+            MQHelperFactory.Default().FanoutConsume(item =>
+            {
+                var mail = JsonConvert.DeserializeObject<MyMail>(item.Item2);
+                MailHelper.SendMail(mail);
+            }, "fanout_Mail", "fanout_queue_Mail", "mail", maxPriority: 100);
+        }
+
+        public void Quit()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ TopShelf.WindowsServers/TopShelf.SendMailServer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+using TopShelf.Job;
+
+namespace TopShelf.SendMailServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                HostFactory.Run(x =>
+                {
+                    x.RunAsLocalSystem();
+
+                    x.SetServiceName("TopShelf.SendMailJob");
+                    x.SetDisplayName("TopShelf.SendMailJob");
+                    x.SetDescription("using topshelf to host windwdos server,processing sendMail");
+
+                    x.Service<SendMailJob>(s =>
+                    {
+                        s.ConstructUsing(() => new SendMailJob());  //设备windowsServer的Job
+                        s.WhenStarted(biz => biz.Process());
+                        s.WhenStopped(biz => biz.Quit());
+                    });
+                });
+            }
+            catch (Exception e)
+            {
+#if DEBUG
+                Console.WriteLine(e.ToString());
+#endif
+
+            }
+        }
+    }
+}
